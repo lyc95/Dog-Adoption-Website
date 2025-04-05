1d053bb7f97456dc08f5c3291bdab3f844986d3f
yzhang4063 <yzhang4063@gatech.edu>
2025-04-06 00:42:31 +0800

    Add page jumps and improve dashboard display logic

---
+++ Phase_3/DogsDatabase/backend/src/main/java/com/example/dogsdatabase/entity/po/UserType.java
@@ -0,0 +1,8 @@
+package com.example.dogsdatabase.entity.po;/**
+ * @Title: UserType
+ * @Author  ttu
+ * @Package com.example.dogsdatabase.entity.po
+ * @Date  2025/4/5 23:01
+ * @description: 
+*/public enum UserType {
+}
