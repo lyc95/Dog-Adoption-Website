3856606636edc49c761f22b80e05ae10147faa70
Li Youcheng <liyouc95@gmail.com>
2025-04-05 21:11:55 +0800

    Updated report views and schema based on Phase 2 comments

--- Phase_3/DogsDatabase/frontend/src/views/ExpenseAnalysis.vue
+++ Phase_3/DogsDatabase/frontend/src/views/ExpenseAnalysis.vue
@@ -1,5 +1,28 @@
 <template>
-    <div>
-        <h1>This is Expense Analysis Report</h1>
-    </div>
+    <h1 >Expense Analysis Report</h1>
+    <el-table :data="tableData" width="100%" >
+        <el-table-column prop="vendorName" label="Vendor Name" />
+        <el-table-column prop="totalSpent" label="Total Spend" />
+    </el-table>
 </template>
+<script setup>
+import request from "@/utils/request.js";
+import { ref, onMounted } from 'vue';
+// Declare a ref to store the table data
+const tableData = ref([]);
+
+// Fetch data from API when the component is mounted
+onMounted(async () => {
+  try {
+    const response = await request({ 
+        url: '/api/report/EAR',
+        method: 'get'
+    });
+    console.log(response)
+    
+    tableData.value = response.data; 
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+});
+</script>
--- Phase_3/DogsDatabase/frontend/src/views/VolunteerLookup.vue
+++ Phase_3/DogsDatabase/frontend/src/views/VolunteerLookup.vue
@@ -1,5 +1,32 @@
 <template>
     <div>
-        <h1>This is Volunteer Lookup function</h1>
+        <h1>Volunteer Lookup</h1>
+        <div>
+            <el-input v-model="input" placeholder="Search Volunteers" clearable /> 
+        </div>
+        <div>
+
+        </div>
     </div>
 </template>
+<script setup>
+import { ref , watch} from 'vue'
+import request from "@/utils/request.js";
+const input = ref('');
+const tableData = ref([]);
+async function handleSearch() {
+  console.log('Searching for:', input.value)
+  try 
+  {
+    const response = await request.get(`/api/report/volunteer/${input.value}`)
+    tableData.value = response.data
+  } catch (error) {
+    console.error('Failed to fetch data:', error)
+    tableData.value = []
+  }
+}
+
+watch(input, (newInput) => {
+  handleSearch(newInput); // Call search handler with the updated input
+})
+</script>
